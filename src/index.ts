a3740d2839f084fea70c39bb5d1930d5b34572ea
n4sunday <dev.nattapon.s@gmail.com>
2022-08-22 03:38:31 +0700

    fix: hot reload nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response } from 'express'
 
-const app: Application = express();
+const app: Application = express()
 
-app.set("port", process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000)
 
-app.get("/", (_req: Request, res: Response) => {
-  res.json({ message: "Hello world!" });
-});
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({ message: 'Hello world Vercel Express!' })
+})
 
-app.listen(app.get("port"), () => {
-  console.log(`Server on http://localhost:${app.get("port")}/`);
-});
+app.listen(app.get('port'), () => {
+  console.log(`Server on http://localhost:${app.get('port')}/`)
+})
